0963a70f3ccb0b62ab6d4f9a0b109d3fd329800b
Sharad Shinde <shindesharad71@gmail.com>
2017-04-02 15:52:12 +0530

    comment section added

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,17 @@ var upload = multer({dest: './public/images'});
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:id', function(req, res, next) {
+  var posts = db.get('posts');
+
+  posts.findById(req.params.id, function(err, post){
+      res.render('show', {
+      'post': post
+    });
+  });
+  
+});
+
 router.get('/add', function(req, res, next) {
   var categories = db.get('categories');
 
@@ -63,6 +74,63 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
         res.location('/');
         res.redirect('/');
       }
+    });
+  }
+});
+
+router.post('/addcomment', function(req, res, next) {
+  // Get form values
+  var name = req.body.name;
+  var email = req.body.email;
+  var body = req.body.body;
+  var postid = req.body.postid;
+  var commentdate = new Date();
+
+
+  // Form Validation
+  req.checkBody('name', 'Name should not empty').notEmpty();
+  req.checkBody('email', 'Email should not empty').notEmpty();
+  req.checkBody('body', 'Body should not empty').notEmpty();
+
+  // Check Errors
+  var errors = req.validationErrors();
+
+  if(errors)
+  {
+    var posts = db.get('posts');
+    posts.findById(postid, function(err, post){
+        res.render('show', {
+      "errors": errors,
+      "post": post
+    });
+    });
+    
+  }
+  else
+  {
+    var comment = {
+      "name": name,
+      "email": email,
+      "body": body,
+      "commentdate": commentdate
+    }
+
+    var posts = db.get('posts');
+    posts.update({"_id": postid},{
+      $push: {
+        "comments": comment
+      }
+    }, function(err, doc){
+        if(err)
+        {
+          throw err;
+        }
+        else
+        {
+          req.flash('success', 'Comment Added');
+          res.location('/posts/show/'+postid);
+          res.redirect('/posts/show/'+postid);
+        }
     });
   }
 });
